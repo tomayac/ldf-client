bf5f86c00e3000fe4acceb6275e990027cfec7af
tomayac <steiner.thomas@gmail.com>
2014-11-21 00:25:02 +0100

    More dynamic parameters for query and start fragment.

--- polymer-ldf-client.js
+++ polymer-ldf-client.js
@@ -4,26 +4,33 @@ Polymer({
   },
 
   executeQuery: function() {
-    var query = 'SELECT ?p ?c WHERE {' +
-          '?p a <http://dbpedia.org/ontology/Artist>.' +
-          '?p <http://dbpedia.org/ontology/birthPlace> ?c.' +
-        '}';
-    var startFragment = 'http://data.linkeddatafragments.org/dbpedia';
+    var that = this;
+    var query = this.query;
+    console.log('Query:\n' + query);
+    var startFragment = this['start-fragment'];
+    console.log('Start Fragment:\n' + startFragment);
+    if (!query || !startFragment) {
+      return;
+    }
+
     var fragmentsClient = new ldf.FragmentsClient(startFragment);
     var results = new ldf.SparqlIterator(query, {
       fragmentsClient: fragmentsClient,
     });
 
     var data = '';
-    var showNext = (function() {
+    (function showNext() {
       var result = results.read();
       if (result === null) {
         results.once('readable', showNext);
       } else {
         data += JSON.stringify(result);
         console.log(JSON.stringify(result));
+        console.log('Fired "query-response"');
+        that.fire('query-response', data);
       }
     })();
+
   }
 
 });
