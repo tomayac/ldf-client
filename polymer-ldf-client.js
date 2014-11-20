aa9a29d7c608972c77b843b63c12472578d95ae0
tomayac <steiner.thomas@gmail.com>
2014-11-20 22:58:37 +0100

    First half-working version.

--- polymer-ldf-client.js
+++ polymer-ldf-client.js
@@ -1,57 +1,29 @@
 Polymer({
-  /**
-   * The `author` attribute sets an initial author
-   *
-   * @attribute author
-   * @type string
-   * @default 'Dimitri Glazkov'
-   */
-  author: 'Dimitri Glazkov',
-
-  /**
-   * `fancy` is a property that does something fancy.
-   *
-   * @property fancy
-   * @type bool
-   * @default false
-   */
-  fancy: false,
-
   ready: function() {
-    // Ready is a lifecycle callback.
-    // You can do setup work in here.
-    // More info: http://www.polymer-project.org/docs/polymer/polymer.html#lifecyclemethods
+    this.executeQuery();
   },
 
-  /**
-   * The `sayHello` method will return a greeting.
-   *
-   * @method sayHello
-   * @param {String} greeting Pass in a specific greeting.
-   * @return {String} Returns a string greeting.
-   */
-  sayHello: function(greeting) {
-    var response = greeting || 'Hello World!';
-    return 'seed-element says, ' + response;
-  },
+  executeQuery: function() {
+    var query = 'SELECT ?p ?c WHERE {' +
+          '?p a <http://dbpedia.org/ontology/Artist>.' +
+          '?p <http://dbpedia.org/ontology/birthPlace> ?c.' +
+        '}';
+    var startFragment = 'http://data.linkeddatafragments.org/dbpedia';
+    var fragmentsClient = new ldf.FragmentsClient(startFragment);
+    var results = new ldf.SparqlIterator(query, {
+      fragmentsClient: fragmentsClient,
+    });
 
-  /**
-   * The `seed-element-lasers-success` event is fired whenever we
-   * call fireLasers.
-   *
-   * @event seed-element-lasers-success
-   * @param {Object} detail
-   *   @param {string} detail.sound An amazing sound.
-   */
-
-  /**
-   * The `fireLasers` method will fire the lasers. At least
-   * it will dispatch an event that claims lasers were fired :)
-   *
-   * @method fireLasers
-   */
-  fireLasers: function() {
-    this.fire('seed-element-lasers-success', { sound: 'Pew pew pew!' });
+    var data = '';
+    var showNext = (function() {
+      var result = results.read();
+      if (result === null) {
+        results.once('readable', showNext);
+      } else {
+        data += JSON.stringify(result);
+        console.log(JSON.stringify(result));
+      }
+    })();
   }
 
 });
